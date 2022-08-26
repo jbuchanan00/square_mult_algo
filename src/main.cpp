c25379b79768072b6245501dfaca4c6316fa049c
Josh Buchanan <josh.buchanan00@gmail.com>
2022-08-25 23:20:12 -0400

    added some ui and the ability to keep requesting expressions until a 'q' is inputted.  need to get some checks in case mod is inputted as a string instead of int. could input as string and try to convert.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <bitset>
 #include <string>
 #include <bit>
+#include <exception>
 
 
 int count_occurences(char t, std::string str) {
@@ -15,11 +16,19 @@ int count_occurences(char t, std::string str) {
 }
 
 int compute_mod(std::string divisor, int mod) {
+	std::string::iterator it;
+	it = std::find(divisor.begin(), divisor.end(), '^');
+	if (it == divisor.end()) {
+		throw std::invalid_argument("Need to include carrot symbol '^' in divisor");
+	}
+	if (it == divisor.begin()) {
+		throw std::invalid_argument("Need a base for the exponent");
+	}
 
 	std::bitset<32> x(std::stoi(divisor.substr(divisor.find("^") + 1)));
 	std::string bit_rep = x.to_string();
 	bit_rep = bit_rep.substr(bit_rep.find("1"));
-	std::cout << bit_rep << std::endl;
+	
 	std::string mod_rep = "";
 
 
@@ -39,16 +48,36 @@ int compute_mod(std::string divisor, int mod) {
 
 int main() {
 
-	std::cout << "Enter the divisor in form ex. 10^10" << std::endl;
-	std::string divisor = "";
-	std::cin >> divisor;
-	std::cout << "Now enter modulus number ex. 10^10 modulus ___" << std::endl;
-	int mod = 0;
-	std::cin >> mod;
-	std::cout << divisor << "%" << mod << std::endl;
+	char quit = ' ';
+	std::cout << "Welcome to this Square and Multipy Algorithm Implementation" << std::endl;
+	std::cout << "If you would like to quit, type q for the divisor input" << std::endl;
+	while (quit != 'q') {
+
+		try {
+			std::cout << "Enter the divisor in form ex. 10^10" << std::endl;
+			std::string divisor = "";
+			std::cin >> divisor;
+
+			if (divisor == "q") {
+				quit = 'q';
+				break;
+			}
+
+			std::cout << "Now enter modulus number ex. 10^10 modulus ___" << std::endl;
+			int mod = 0;
+			std::cin >> mod;
+			
+			int result = compute_mod(divisor, mod);
+			std::cout << "Expression: " << divisor << "%" << mod << "=" << result << std::endl;
+		}
+		catch (const std::exception& e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
 	
-	int result = compute_mod(divisor, mod);
-	std::cout << result << std::endl;
+	
+
+
 
 	return 0;
 }
